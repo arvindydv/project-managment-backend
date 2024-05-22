0f6477d2f469a9e70443753bab5ea720e247df99
arvindydv <arvindydv03@gmail.com>
2024-05-22 17:12:09 +0530

    sprints api completed

--- controllers/sprint.controller.js
+++ controllers/sprint.controller.js
@@ -4,8 +4,20 @@ const models = require("../models");
 const moment = require("moment");
 const { response } = require("express");
 
-// Check user designation
-const isManager = async () => {
+// create sprint
+const createSprint = asyncHandler(async (req, res) => {
+  const payload = req.body;
+  if (
+    !payload.name ||
+    !payload.description ||
+    !payload.deadline ||
+    !payload.workspaceId
+  ) {
+    return res
+      .status(400)
+      .send(new ApiResponse(400, {}, "All fields are required"));
+  }
+
   const UserDesignation = await models.Designation.findOne({
     where: {
       designationCode: "101",
@@ -22,46 +34,11 @@ const isManager = async () => {
     },
   });
 
-  return userWorkspaceMapping;
-};
-
-// create sprint
-const createSprint = asyncHandler(async (req, res) => {
-  const payload = req.body;
-  if (
-    !payload.name ||
-    !payload.description ||
-    !payload.deadline ||
-    !payload.workspaceId
-  ) {
-    return res
-      .status(400)
-      .send(new ApiResponse(400, {}, "All fields are required"));
-  }
-
-  //   const UserDesignation = await models.Designation.findOne({
-  //     where: {
-  //       designationCode: "101",
-  //     },
-  //   });
-
-  //   const userWorkspaceMapping = await models.UserWorkspaceMapping.findOne({
-  //     where: {
-  //       workspaceId: workspace.dataValues.id,
-  //       [Op.and]: [
-  //         { userId: req.user.id },
-  //         { designationId: UserDesignation.dataValues.id },
-  //       ],
-  //     },
-  //   });
-
-  const checkUserIsManager = isManager();
-
-  if (!checkUserIsManager) {
+  if (!userWorkspaceMapping) {
     return res
       .status(401)
       .json(
-        new ApiResponse(401, {}, "You can not create sprint in this workspace.")
+        new ApiResponse(401, {}, "You cannot create sprint in this workspace")
       );
   }
 
@@ -88,15 +65,29 @@ const updateSprint = asyncHandler(async (req, res) => {
     return res.status(404).json(new ApiResponse(404, {}, "Sprint not found"));
   }
 
-  const checkUserIsManager = isManager();
-  if (!checkUserIsManager) {
+  const UserDesignation = await models.Designation.findOne({
+    where: {
+      designationCode: "101",
+    },
+  });
+
+  const userWorkspaceMapping = await models.UserWorkspaceMapping.findOne({
+    where: {
+      workspaceId: findSprint.dataValues.workspaceId,
+      [Op.and]: [
+        { userId: req.user.id },
+        { designationId: UserDesignation.dataValues.id },
+      ],
+    },
+  });
+
+  if (!userWorkspaceMapping) {
     return res
       .status(401)
       .json(
-        new ApiResponse(401, {}, "You can not update sprint in this workspace.")
+        new ApiResponse(401, {}, "You cannot update sprint in this workspace")
       );
   }
-
   const sprint = await models.Sprint.update(payload, {
     where: { id: sprintId },
   });
@@ -107,10 +98,79 @@ const updateSprint = asyncHandler(async (req, res) => {
 });
 
 // delete  sprint
-const deleteSPrint = asyncHandler(async (req, res) => {});
+const deleteSPrint = asyncHandler(async (req, res) => {
+  const sprintId = req.params.sprintId;
+
+  const findSprint = await models.Sprint.findOne({ id: sprintId });
+  if (!findSprint) {
+    return res.status(404).json(new ApiResponse(404, {}, "Sprint not found"));
+  }
+
+  const UserDesignation = await models.Designation.findOne({
+    where: {
+      designationCode: "101",
+    },
+  });
+
+  const userWorkspaceMapping = await models.UserWorkspaceMapping.findOne({
+    where: {
+      workspaceId: findSprint.dataValues.workspaceId,
+      [Op.and]: [
+        { userId: req.user.id },
+        { designationId: UserDesignation.dataValues.id },
+      ],
+    },
+  });
+
+  if (!userWorkspaceMapping) {
+    return res
+      .status(401)
+      .json(
+        new ApiResponse(401, {}, "You cannot delete sprint in this workspace")
+      );
+  }
+
+  await models.Sprint.destroy({
+    where: {
+      id: sprintId,
+    },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "sprint deleted successfully"));
+});
+
+const getAllSprints = asyncHandler(async (req, res) => {
+  const workspaceId = req.params.workspaceId;
+  const page = parseInt(req.query.page) || 0;
+  const limit = parseInt(req.query.limit) || 10;
+  const offset = page * limit;
+
+  const sprints = await models.Sprint.findAll({
+    where: { workspaceId: workspaceId },
+    attributes: {
+      exclude: ["created_at", "updated_at", "deleted_at"],
+    },
+    include: [
+      {
+        model: models.Task,
+        as: "Task",
+        attributes: ["task", "description", "deadline", "status", "userId"],
+      },
+    ],
+    limit: limit,
+    offset: offset,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, sprints, "Sprint retrieved successfully"));
+});
 
 module.exports = {
   createSprint,
   updateSprint,
   deleteSPrint,
+  getAllSprints,
 };
--- routes/sprints.route.js
+++ routes/sprints.route.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const {
   createSprint,
   updateSprint,
+  deleteSPrint,
+  getAllSprints,
 } = require("../controllers/sprint.controller");
 
 const { verifyToken } = require("../middlewares/auth.middleware");
@@ -10,7 +12,7 @@ const router = Router();
 
 router.post("/sprint", verifyToken, createSprint);
 router.patch("/sprint/:sprintId", verifyToken, updateSprint);
-// router.delete("/workspace/:workspaceId", verifyToken, updateWorkspace);
-// router.delete("/remove-user/", verifyToken, removeUserFromWorkspace);
+router.delete("/sprint/:sprintId", verifyToken, deleteSPrint);
+router.get("/sprint", verifyToken, getAllSprints);
 
 module.exports = router;
